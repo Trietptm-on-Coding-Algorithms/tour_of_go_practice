93f50ac656c4b24a9f75f85f725766eaebd50bf9
tkmru <i.am.tkmru@gmail.com>
2016-11-11 23:55:15 +0900

    I want to pwn a girl.

--- 05_04_pointer_receivers/methods-pointers.go
+++ 05_04_pointer_receivers/methods-pointers.go
@@ -9,12 +9,17 @@ type Vertex struct {
     X, Y float64
 }
 
-// メソッドは、レシーバ引数を伴う関数
 func (v Vertex) Abs() float64 {
     return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func (v *Vertex) Scale(f float64) {
+    v.X = v.X * f
+    v.Y = v.Y * f
+}
+
 func main() {
     v := Vertex{3, 4}
+    v.Scale(10)
     fmt.Println(v.Abs())
 }
--- 05_07_methods_and_pointer_indirection2/indirection-values.go
+++ 05_07_methods_and_pointer_indirection2/indirection-values.go
@@ -9,12 +9,20 @@ type Vertex struct {
     X, Y float64
 }
 
-// メソッドは、レシーバ引数を伴う関数
 func (v Vertex) Abs() float64 {
+    return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func AbsFunc(v Vertex) float64 {
     return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
     v := Vertex{3, 4}
     fmt.Println(v.Abs())
+    fmt.Println(AbsFunc(v))
+
+    p := &Vertex{4, 3}
+    fmt.Println(p.Abs())
+    fmt.Println(AbsFunc(*p))
 }
